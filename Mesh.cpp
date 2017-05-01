d2de5154cba51c68de2dbb0cb2daf759ea3ab53f
polmes <pol.developer1@gmail.com>
2017-05-02 00:13:31 +0200

    Warming up with c++ classes

---
+++ Mesh.cpp
@@ -0,0 +1,53 @@
+// C++ libraries
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <string>
+#include <vector>
+
+class Mesh {
+
+private:
+	std::vector<double> line;
+	std::string name;
+
+public:
+	// Constructor
+	Mesh(uint8_t, std::string);
+
+	// Destructor
+	// ~Mesh();
+
+	// Methods
+	void printVector() {
+		for (uint8_t i = 0; i < line.size(); ++i) {
+			std::cout << line[i] << std::endl;
+		}
+	}
+	void anotherMethod();
+};
+
+Mesh::Mesh(uint8_t N, std::string str): name(str) {
+	for (uint8_t i = 0; i < N; ++i) {
+		line.push_back(i+1);
+	}
+}
+
+void Mesh::anotherMethod() {
+	std::cout << "Size: " << line.size() << std::endl; // Mesh::line, too
+	std::cout << "Name: " << name << std::endl;
+}
+
+const uint8_t N = 50;
+
+int main(int argc, char const *argv[])
+{
+	Mesh mesh(N, "damn");
+	mesh.printVector();
+	mesh.anotherMethod();
+	// mesh.anotherMethod();
+
+	return 0;
+}
