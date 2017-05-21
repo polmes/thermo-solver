d08d93af4a1a79446c017fa16b811dddcb2d579c
polmes <pol.developer1@gmail.com>
2017-05-21 02:50:38 +0200

    Add test main file

---
+++ main.cpp
@@ -0,0 +1,20 @@
+// C++ libraries
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <string>
+#include <vector>
+
+// My libraries
+#include "mesh.hpp"
+
+// Read config file and get all consts
+
+int main(int argc, char const *argv[]) {
+	Mesh mesh(M, N);
+	mesh.printMatrix();
+
+	return 0;
+}
