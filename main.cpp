50dd7684119a39c959e08abcd00fed39f07ce11a
polmes <pol.developer1@gmail.com>
2017-06-29 19:56:37 +0200

    New libraries and argc exceptions

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include <cmath>
 #include <string>
 #include <vector>
+#include <thread>
+#include <stdexcept>
 
 // My libraries
 #include "mesh.hpp"
@@ -13,6 +15,11 @@
 // Read config file and get all consts
 
 int main(int argc, char** argv) {
+	if (argc < 1 || argc > 2) {
+		throw std::invalid_argument("ERROR: Unexpected number of arguments.");
+		return 1;
+	}
+
 	Mesh mesh(M, N);
 	mesh.printMatrix();
 
