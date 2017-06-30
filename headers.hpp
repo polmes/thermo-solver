279f9e633ed17fb69846cf3ecdc9df0af69ccfd7
polmes <pol.developer1@gmail.com>
2017-07-01 00:02:42 +0200

    All common C++ standard libraries in one header file

---
+++ headers.hpp
@@ -0,0 +1,34 @@
+// Standard libraries
+#ifndef INCLUDE_STL
+	#include <cstdlib>
+	#include <iostream>
+	#include <fstream>
+	#include <sstream>
+	#include <cmath>
+	#include <string>
+	#include <vector>
+	#include <algorithm>
+	#include <thread>
+	#include <stdexcept>
+	#define INCLUDE_STL
+#endif
+
+/*
+// My libraries
+#ifndef INCLUDE_MESH
+	#include "mesh.hpp"
+	#define INCLUDE_MESH
+#endif
+#ifndef INCLUDE_VOLUME
+	#include "volume.hpp"
+	#define INCLUDE_VOLUME
+#endif
+#ifndef INCLUDE_MATERIAL
+	#include "material.hpp"
+	#define INCLUDE_MATERIAL
+#endif
+#ifndef INCLUDE_UTIL
+	#include "util.hpp"
+	#define INCLUDE_UTIL
+#endif
+*/
