8933c4f539952ad04dd58c655e983a9b121423c8
polmes <pol.developer1@gmail.com>
2017-06-30 17:11:15 +0200

    Basic Volume class

--- volume.hpp
+++ volume.hpp
@@ -1,10 +1,14 @@
 class Volume {
 	private:
-		static const int M, N; // shared by all Volume instances
+		// static const int M, N; // shared by all Volume instances
+		double aE, aW, aN, aS, aP, bP;
+		Material material;
+		bool isBoundary; // boundary = 'w' 'e' 'n' 's' 0
 
 	protected:
 		//
 
 	public:
-		//
-}
+		// Constructor
+		Volume();
+};
