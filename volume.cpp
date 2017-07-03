e1db823b7d4a55adeff988818a6564d076644fb9
polmes <pol.developer1@gmail.com>
2017-07-03 16:26:50 +0200

    Tried to use vectors of pointers

--- volume.cpp
+++ volume.cpp
@@ -6,22 +6,60 @@
 	#include "volume.hpp"
 	#define INCLUDE_VOLUME
 #endif
+#ifndef INCLUDE_UTIL
+	#include "util.hpp"
+	#define INCLUDE_UTIL
+#endif
 
-Volume::Volume(const std::vector< std::vector<double> > &X, const double &depth, const std::vector<std::vector<double>::size_type> &ij, const std::vector<unsigned int> &N) {
+Volume::Volume(const std::vector< std::vector<double> > &X, const double &depth, const std::vector<std::vector<double>::size_type> &ij, const std::vector<unsigned int> &N, const std::vector< std::vector<Condition> > &_conditions) {
 	x = {(X[0][0] + X[0][1]) / 2, (X[1][0] + X[1][1]) / 2};
 	d = {x[0] - X[0][0], x[1] - X[1][0]};
 	S = {2 * d[1] * depth, 2 * d[0] * depth};
 	V = (2 * d[0]) * (2 * d[1]) * depth;
 	
-	isBoundary = {{false, false}, {false, false}}; // {{W, E}, {S, N}}
+	isBoundary = false;
+	conditions.resize(N.size(), std::vector<const Condition*>(N.size(), nullptr)); // {{W, E}, {S, N}}
 	for (std::vector<unsigned int>::size_type i = 0; i < N.size(); i++) {
-		if (ij[i] == 0) isBoundary[i][0] = true;
-		else if (ij[i] == N[i]) isBoundary[i][1] = true;
+		if (ij[i] == 0) {
+			conditions[i][0] = &_conditions[i][0];
+			isBoundary = true;
+		} else if (ij[i] == N[i]) {
+			conditions[i][1] = &_conditions[i][1];
+			isBoundary = true;
+		}
+	}
+
+	// Initialize coefficients
+	neighbors.resize(N.size(), std::vector<const Volume*>(N.size(), nullptr));
+	std::cout << neighbors.size() << " " << neighbors[0].size() << std::endl;
+	std::cout << neighbors[0][0] << " " << neighbors[0][1] << std::endl;
+
+	aP = bP = 0.0;
+	a = {{0.0, 0.0}, {0.0, 0.0}};
+}
+
+Volume::~Volume() {
+	std::vector<const Condition*>::size_type size, sizes;
+	sizes = conditions.size();
+	for (std::vector<const Condition*>::size_type i = 0; i < sizes; i++) {
+		size = conditions[i].size();
+		for (std::vector<const Condition*>::size_type j = 0; j < size; j++) {
+			delete conditions[i][j];
+			delete neighbors[i][j];
+		}
 	}
 }
 
 std::vector<double> Volume::get_x() const {
 	return x;
+}
+
+std::vector<double> Volume::get_d() const {
+	return d;
+}
+
+double Volume::get_V() const {
+	return V;
 }
 
 const Material* Volume::get_material() const {
@@ -32,82 +70,130 @@ void Volume::set_material(const Material *_material) {
 	material = _material;
 }
 
-double Volume::get_aE() const {
-	return aE;
+void Volume::set_neighbors(const std::vector<std::vector<double>::size_type> &ij, const std::vector< std::vector<Volume> > &volumes) {
+	std::cout << "SETTING UP" << std::endl;
+	std::cout << conditions.size() << std::endl;
+	neighbors.resize(2);
+	std::cout << neighbors.size() << std::endl;
+	neighbors[0].resize(2);
+	std::cout << neighbors[0].size() << std::endl;
+	// neighbors.resize(conditions.size());
+
+	// std::cout << "AGAIN" << std::endl;
+	// std::cout << &(volumes[0][0]) << std::endl;
+	// // neighbors.push_back()
+	// std::cout << "0SZ: " << neighbors[0].size() << std::endl;
+	// std::vector<const Volume*> test;
+	// neighbors.resize(neighbors.size(), std::vector<const Volume*>(neighbors.size(), nullptr));
+
+	// test.push_back(&(volumes[0][0]));
+	// std::cout << "test" << std::endl;
+	// neighbors.push_back(test);
+	// std::cout << "NOPE" << std::endl;
+
+	// for (std::vector<const Volume*>::size_type i = 0; i < neighbors.size(); i++) {
+	// 	// neighbors[i].resize(conditions[i].size());
+	// 	std::vector<const Volume*> neighbors1D;
+	// 	for (std::vector<const Volume*>::size_type j = 0; j < neighbors[i].size(); j++) {
+	// 		std::cout << "here" << std::endl;
+	// 		if (conditions[i][j] == NULL) {
+	// 			std::cout << "there" << std::endl;
+	// 			if (i == 0) {
+	// 				std::cout << "tthere" << std::endl;
+	// 				if (j == 0) neighbors1D.emplace_back(&(volumes[ij[0]-1][ij[1]]));
+	// 				else neighbors1D.emplace_back(&(volumes[ij[0]+1][ij[1]])); // j == 1
+	// 			} else { // i == 1
+	// 				if (j == 0) neighbors1D.emplace_back(&(volumes[ij[0]][ij[1]-1]));
+	// 				else neighbors1D.emplace_back(&(volumes[ij[0]][ij[1]+1])); // j == 1
+	// 			}
+	// 		} else {
+	// 			std::cout << "also" << std::endl;
+	// 			neighbors1D.push_back(nullptr);
+	// 		}
+	// 	}
+	// 	std::cout << "final push" << std::endl;
+	// 	neighbors.push_back(neighbors1D);
+	// 	std::cout << "final push" << std::endl;
+	// }
 }
 
-void Volume::set_aE(const double &_aE) {
-	aE = _aE;
+void Volume::computeCoefficients(const double &beta, const double &tDelta, const double &t, const double &Tprev, const std::vector< std::vector<double> > &Tneighbors) {
+
+	// Inner volumes
+	if (!isBoundary) {
+		std::cout << "INNER" << std::endl;
+
+		aP = this->get_material()->get_rho() * this->get_material()->get_cp() * this->get_V() / tDelta;
+		bP = this->get_material()->get_rho() * this->get_material()->get_cp() * this->get_V() * Tprev / tDelta + beta * this->get_material()->get_qv() * this->get_V();
+
+		for (std::vector<Volume>::size_type i = 0; i < neighbors.size(); i++) {
+			for (std::vector<Volume>::size_type j = 0; j < neighbors[i].size(); j++) {
+				a[i][j] = this->computeLambda(i, *neighbors[i][j]) * S[i] / (this->get_d()[i] + neighbors[i][j]->get_d()[i]);
+				bP += (1 - beta) * a[i][j] * (Tneighbors[i][j] - Tprev);
+				a[i][j] = beta * a[i][j];
+				aP += a[i][j];
+			}
+		}
+	}
+
+	// Boundary volumes
+	else {
+		std::cout << "OUTER" << std::endl;
+		std::cout << "CONDITIONS SIZE: " << conditions.size() << std::endl;
+
+		// Isotherm
+		bP = 0.0;
+		unsigned int k = 0;
+		for (std::vector<Condition*>::size_type i = 0; i < conditions.size(); i++) {
+			for (std::vector<Condition*>::size_type j = 0; j < conditions[i].size(); j++) {
+				if (conditions[i][j] != NULL && conditions[i][j]->get_conditionType() == ISOTHERM) {
+					std::cout << "made it" << std::endl;
+					bP += conditions[i][j]->get_T(t);
+					k++;
+				}
+			}
+		}
+		if (k > 0) {
+			aP = 1.0;
+			bP = bP/k; // average if is a corner volume
+			a = {{0.0, 0.0}, {0.0, 0.0}};
+		}
+
+		// Convection & Qflow
+		else { // k == 0
+			aP = this->get_material()->get_rho() * this->get_material()->get_cp() * this->get_V() / tDelta;
+			bP = this->get_material()->get_rho() * this->get_material()->get_cp() * this->get_V() * Tprev / tDelta + beta * this->get_material()->get_qv() * this->get_V();
+
+			for (std::vector<Volume>::size_type i = 0; i < conditions.size(); i++) { // neighbors.size()
+				for (std::vector<Volume>::size_type j = 0; j < conditions[i].size(); j++) { // neighbors[i].size()		// Inner side
+					if (conditions[i][j] == NULL) {
+						a[i][j] = this->computeLambda(i, *neighbors[i][j]) * S[i] / (this->get_d()[i] + neighbors[i][j]->get_d()[i]);
+						bP += (1 - beta) * a[i][j] * (Tneighbors[i][j] - Tprev);
+						a[i][j] = beta * a[i][j];
+						aP += a[i][j];
+					}
+
+					// Boundary side
+					else {
+						a[i][j] = 0;
+						if (conditions[i][j]->get_conditionType() == CONVECTION) {
+							aP += beta * conditions[i][j]->get_alpha() * S[i];
+							bP += aP * conditions[i][j]->get_Tg() + (1 - beta) * conditions[i][j]->get_alpha() * (conditions[i][j]->get_Tg() - Tprev) * S[i];
+						}
+						else if (conditions[i][j]->get_conditionType() == FLOW) {
+							bP += conditions[i][j]->get_Qflow() * this->get_d()[!i] * 2; // beta * Qflow + (1 - beta) * Qflow
+						}
+					}
+				}
+			}
+		}
+	}
+
+	std::cout << "aP = " << aP << std::endl;
+	std::cout << "bP = " << bP << std::endl;
+	printMatrix(a);
 }
 
-double Volume::get_aW() const {
-	return aW;
-}
-
-void Volume::set_aW(const double &_aW) {
-	aW = _aW;
-}
-
-double Volume::get_aN() const {
-	return aN;
-}
-
-void Volume::set_aN(const double &_aN) {
-	aN = _aN;
-}
-
-double Volume::get_aS() const {
-	return aS;
-}
-
-void Volume::set_aS(const double &_aS) {
-	aS = _aS;
-}
-
-double Volume::get_aP() const {
-	return aP;
-}
-
-void Volume::set_aP(const double &_aP) {
-	aP = _aP;
-}
-
-double Volume::get_bP() const {
-	return bP;
-}
-
-void Volume::set_bP(const double &_bP) {
-	bP = _bP;
-}
-
-// void Volume::computeAllCoefficients(const std::vector< std::vector<Volume> > &neighbors, const double &beta, const double &tDelta, const double &t, const double &tPrev) {
-// 	std::vector< std::vector<double> > lambdas;
-
-// 	for (std::vector<Volume>::size_type i = 0; i < neighbors.size(); i++) {
-// 		for (std::vector<Volume>::size_type j = 0; j < neighbors[i].size(); j++) {
-// 			lambdas[i][j] = (this->get_d()[i] + neighbors[i][j].get_d()[i]) / (this->get_d()[i]/this->get_material().get_lambda() + neighbors[i][j].get_d()[i]/neighbors[i][j].get_material().get_lambda());
-// 		}
-// 	}
-
-// 	aW = beta * lambdas[0][0] * S[0] / (this->get_d()[0] + neighbors[0][0].get_d()[0]);
-// 	aE = beta * lambdas[0][1] * S[0] / (this->get_d()[0] + neighbors[0][1].get_d()[0]);
-// 	aS = beta * lambdas[1][0] * S[1] / (this->get_d()[1] + neighbors[1][0].get_d()[1]);
-// 	aN = beta * lambdas[1][1] * S[1] / (this->get_d()[1] + neighbors[1][1].get_d()[1]);
-
-// 	bP = this->get_material().get_rho() * this->get_material().get_cp() * this->get_V() * this->get_Tprev() / tDelta + beta * this->get_material().get_qv(t) * this->get_V() + (1 - beta) * (aE * (neighbors[0][1].get_Tprev() - this->get_Tprev()) - aW * (this->get_Tprev() - neighbors[0][0].get_Tprev()) + aN * (neighbors[1][1].get_Tprev() - this->get_Tprev()) - aS * (this->get_Tprev() - neighbors[1][0].get_Tprev()) + this->get_qv(tPrev) * this->get_V());
-
-// 	aW = beta * aW;
-// 	aE = beta * aE;
-// 	aS = beta * aS;
-// 	aN = beta * aN;
-
-// 	aP = this->get_material().get_rho() * this->get_material().get_cp() * this->get_V() / tDelta + aW + aE + aS + aN;
-// }
-
-void Volume::computeTransientCoefficients() {
-
-}
-
-std::vector< std::vector<double> > Volume::computeLambdas() {
-
+double Volume::computeLambda(const std::vector<Volume>::size_type &i, const Volume &neighbor) {
+	return (this->get_d()[i] + neighbor.get_d()[i]) / (this->get_d()[i]/this->get_material()->get_lambda() + neighbor.get_d()[i]/neighbor.get_material()->get_lambda());
 }
--- volume.hpp
+++ volume.hpp
@@ -6,42 +6,43 @@
 	#include "material.hpp"
 	#define INCLUDE_MATERIAL
 #endif
+#ifndef INCLUDE_CONDITION
+	#include "condition.hpp"
+	#define INCLUDE_CONDITION
+#endif
 
 class Volume {
 	private:
 		std::vector<double> x; // node coordinates
 		std::vector<double> S; // {vertical, horizontal} areas
 		std::vector<double> d; // {horizontal, vertical} distance to volume boundary
-		std::vector< std::vector<bool> > isBoundary; // {{W, E}, {S, N}}
+		std::vector< std::vector<const Condition*> > conditions; // {{W, E}, {S, N}}
+		bool isBoundary; // is a boundary volume?
 		double V; // volume volume
-		const Material *material;
-		double aE, aW, aN, aS, aP, bP; // coefficients
+		const Material *material; // material properties: rho, cp, lambda, qv
+		double aP, bP; // coefficients
+		std::vector< std::vector<double> > a; // more coefficients: {{aW, aE}, {aS, aN}}
+		// std::vector< std::vector<const Volume*> > neighbors; // surrounding volumes
 
 	public:
-		// Constructor
-		Volume(const std::vector< std::vector<double> > &X, const double &depth, const std::vector<std::vector<double>::size_type> &ij, const std::vector<unsigned int> &N);
+		std::vector< std::vector<const Volume*> > neighbors; // surrounding volumes
+		// Constructors
+		Volume(const std::vector< std::vector<double> > &X, const double &depth, const std::vector<std::vector<double>::size_type> &ij, const std::vector<unsigned int> &N, const std::vector< std::vector<Condition> > &_conditions);
+
+		// Destructors
+		~Volume(); // must delete pointers
 
 		// Getters
-		double get_aE() const;
-		double get_aW() const;
-		double get_aN() const;
-		double get_aS() const;
-		double get_aP() const;
-		double get_bP() const;
 		std::vector<double> get_x() const;
+		std::vector<double> get_d() const;
+		double get_V() const;
 		const Material* get_material() const;
 
 		// Setters
-		void set_aE(const double &_aE);
-		void set_aW(const double &_aW);
-		void set_aN(const double &_aN);
-		void set_aS(const double &_aS);
-		void set_aP(const double &_aP);
-		void set_bP(const double &_bP);
 		void set_material(const Material *_material);
+		void set_neighbors(const std::vector<std::vector<double>::size_type> &ij, const std::vector< std::vector<Volume> > &volumes);
 
 		// Methods
-		void computeAllCoefficients(const std::vector< std::vector<Volume> > &neighbors, const double &beta, const double &tDelta, const double &t, const double &tPrev);
-		void computeTransientCoefficients();
-		std::vector< std::vector<double> > computeLambdas();
+		void computeCoefficients(const double &beta, const double &tDelta, const double &t, const double &Tprev, const std::vector< std::vector<double> > &Tneighbors);
+		double computeLambda(const std::vector<Volume>::size_type &i, const Volume &neighbor);
 };
